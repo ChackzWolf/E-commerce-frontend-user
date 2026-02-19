32e02f60e8f8b1f3816109c42dc633d88bdb65f3
Jackson-Problink <138844080+ChackzWolf@users.noreply.github.com>
2026-02-19 11:00:17 +0530

    updated base url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL =   import.meta.env.VITE_BASE_URL  || "https://e-commerce-server-o9u1.onrender.com";
+const API_BASE_URL =   import.meta.env.VITE_BASE_URL  || "https://e-commerce-server-o9u1.onrender.com/api";
 
 const getHeader = () => {
     const token = localStorage.getItem("accessToken");
