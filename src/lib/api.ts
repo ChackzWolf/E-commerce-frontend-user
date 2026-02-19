10c68cc09f37c57d55b8f1d481fb4e5c1876eb28
Jackson-Problink <138844080+ChackzWolf@users.noreply.github.com>
2026-02-19 10:01:37 +0530

    changed base url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL =process.env.BASE_URL || "https://e-commerce-server-o9u1.onrender.com";
 
 const getHeader = () => {
     const token = localStorage.getItem("accessToken");
